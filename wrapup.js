2c62b55dd42b781068fb04bc5f919ac8484bf5ed
dunjamarc <dunjamarkovic86@gmail.com>
2018-02-11 00:26:24 +0100

    finished

--- wrapup.js
+++ wrapup.js
@@ -1,12 +1,12 @@
 // spajanje sortiranih nizova
-function merge(a, b){
+function merge(a, b) {
     var i;
     var j;
     var k;
     var c = [];
 
-    for(i = 0, j = 0, k = 0; i < a.length && j < b.length; k++){
-        if(a[i] < b[j]){
+    for (i = 0, j = 0, k = 0; i < a.length && j < b.length; k++) {
+        if (a[i] < b[j]) {
             c[k] = a[i];
             i++;
         } else {
@@ -15,13 +15,13 @@ function merge(a, b){
         }
     }
 
-    if(i == a.length){     // ako smo na kraju a
-        for( /*j=j*/ ; j < b.length; j++){
+    if (i == a.length) {     // ako smo na kraju a
+        for ( /*j=j*/; j < b.length; j++) {
             c[k] = b[j];
             k++;
         }
     } else {               // ako smo na kraju b
-        for( /*i=i*/ ; i < a.length; i++){
+        for ( /*i=i*/; i < a.length; i++) {
             c[k] = a[i];
             k++;
         }
@@ -29,25 +29,25 @@ function merge(a, b){
     return c;
 }
 
-console.log(merge([1,3,5],[1,2,4,6]));
+console.log(merge([1, 3, 5], [1, 2, 4, 6]));
 
 //-------------------nadovezati dva niza nesortirana
 
-function merge1(a, b){
-    for(var i = 0; i < b.length; i++){
-        a[a.length] = b[i]; 
+function merge1(a, b) {
+    for (var i = 0; i < b.length; i++) {
+        a[a.length] = b[i];
     }
     return a;
 }
 
-console.log(merge1([1,3,5],[7,2,4,6]));
+console.log(merge1([1, 3, 5], [7, 2, 4, 6]));
 
 //------------------------------------------
 
-function merging(a, b){
+function merging(a, b) {
     var c = [];
     var j = 0;
-    for(i = 0; i < a.length; i++){
+    for (i = 0; i < a.length; i++) {
         c[j] = a[i];
         j++;
         c[j] = b[i];
@@ -55,22 +55,72 @@ function merging(a, b){
     }
     return c;
 }
-console.log(merging([1,3,5],[7,2,4]));
+console.log(merging([1, 3, 5], [7, 2, 4]));
 
 //-----------------------------------------9
 
-function separator(str, a){
-    var str1 = "";
-    for(var i = 0; i < str.length; i++){
-        if(str[i] === " "){
-            str1 = str1 + a;
-        } else  if (a == undefined){
-            str1 = str1 + "-";
+function separator(str, a) {
+    var newStr = "";
+    if (a === undefined) {
+        a = "-";
+    }
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] === " ") {
+            newStr = newStr + a;
         } else {
-            str1 = str1 + a; 
+            newStr = newStr + str[i];
         }
     }
-    return str1;
+    return newStr;
 }
 
-console.log(separator("My random string",));
+console.log(separator("My random string"));
+
+//-------------------------------------------
+
+function partOfString(str, n) {
+    var newStr = "";
+    for (var i = 0; i < n; i++) {
+        newStr = newStr + str[i];
+    }
+    return newStr + "...";
+}
+
+console.log(partOfString("tralala", 5));
+
+//---------------------------------------------12
+
+function retire(n, sex){
+    var age = 2018 - n;
+    if(sex == "m"){
+        if(age < 65){
+            return (65 - age);
+        } else {
+            return "You are already retired."
+        }
+    } else if(sex == "f") {
+        if(age < 60){
+            return (60 - age);
+        } else {
+            return "You are already retired."
+        }
+    }
+}
+
+console.log(retire(1961, "f"));
+
+//--------------------------------------------13
+
+function first(num){
+    if(num == 1 || num % 10 == 1 && num !== 11){
+        return (num + "st");
+    } else if(num == 2 || num % 10 == 2 && num !== 12){
+        return (num + "nd");
+    } else if(num == 3 || num % 10 == 3 && num !== 13){
+        return (num + "rd");
+    } else {
+        return (num + "th");
+    }
+}
+
+console.log(first(34456));
